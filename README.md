e47ea425dfab24b5f6c549952dc870f83de20a02
Alexander Evstratyev <alexander.evstratyev@gmail.com>
2013-06-29 10:46:56 +0300

    Repace project into git

---
+++ README.md
@@ -0,0 +1,3 @@
+new Project for pptp users.
+
+
---
+++ src/main/java/net/odtel/usercheck/controller/CurrentUserController.java
@@ -0,0 +1,30 @@
+package net.odtel.usercheck.controller;
+
+import java.util.List;
+
+import net.odtel.usercheck.model.CurrentUser;
+import net.odtel.usercheck.service.CurrentUserService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+@Controller
+public class CurrentUserController {
+
+	@Autowired
+	private CurrentUserService currentUserService;
+	
+	@RequestMapping(value="/currentUsers", method = RequestMethod.GET)
+	String getListController (Model model){
+		
+		List<CurrentUser> currentUsers = currentUserService.findAll();
+		
+		model.addAttribute("currentUsers", currentUsers);
+		
+		return "currentUsers";
+	}
+}
